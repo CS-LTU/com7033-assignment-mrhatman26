7bd339fa95e81667e70a9f5f839316104ab81179
mrhatman26 <{ID}+{username}@users.noreply.github.com>
2024-11-03 16:35:16 +0000

    Commented db_reader.py and healthdb.sql

--- db_reader.py
+++ db_reader.py
@@ -12,24 +12,26 @@ def str_to_booInt(YesNo):
             return 1
         else:
             return 0
+    #A simple function to convert "Yes" "No" strings to their boolean counterparts.
+    #YesNo = The text to convert to a bool. (String)
 
     
 def read_presaved_data(mongodb, mysql, m_client):
-    with open("healthcare-dataset-stroke-data.csv") as csvfile:
-        reader = csv.reader(csvfile, delimiter=" ")
-        data = list(reader)
-        row_count = len(data)
+    with open("healthcare-dataset-stroke-data.csv") as csvfile: #Open the CSV file as csvfile.
+        reader = csv.reader(csvfile, delimiter=" ") #Create an instance of CSV Reader on the CSV File.
+        data = list(reader) #Convert the reader to a list.
+        row_count = len(data) #Get the total length of the file.
         index = 0
         for row in data:
-            if len(row) > 1:
+            if len(row) > 1: #Sometimes, the rows in the file are split into two. If this is the case, join them back together.
                 row = row[0] + row[1]
-                row = row.split(",")
+                row = row.split(",") #Split the row into 12 items using the commas as a seperator.
             else:
-                row = row[0].split(",")
-            if row[9] == "N/A":
+                row = row[0].split(",") #Split the row into 12 items using the commas as a seperator.
+            if row[9] == "N/A": #If the BMI is equal to "N/A", set it to None.
                 row[9] = None
-            row[10] = row[10].replace("smoked", " smoked")
-            row_dict = {
+            row[10] = row[10].replace("smoked", " smoked") #If the smoked data does not contain a space, add one.
+            row_dict = { #Add all of the data to a dictionary so it can easily be saved to the database.
                 "patient_gender": row[1].upper(),
                 "patient_age": row[2].upper(),
                 "patient_hyperT": str_to_booInt(row[3]),
@@ -42,16 +44,17 @@ def read_presaved_data(mongodb, mysql, m_client):
                 "patient_smoked": row[10].upper(),
                 "patient_stroke": str_to_booInt(row[11].upper())
             }
-            if row[0] != "id":
-                if mysql is True:
+            if row[0] != "id": #Make sure the headers aren't saved.
+                if mysql is True: #If MySQL is True, save the data to MySQL.
                     mysql_id = insert_patient_data(row_dict)
                 else:
                     mysql_id = -99
-                if mongodb is True:
+                if mongodb is True: #If mongodb is True, save the data to MongoDB.
                     row_dict["MySQL_ID"] = mysql_id
                     mongo_insert(row_dict, m_client)
             print(str(index) + "/" + str(row_count) + " rows loaded from healthcare-dataset-stroke-data.csv", flush=True, end="\r")
+            #Print the progress of the reading of the file.
             index += 1
-    csvfile.close()
+    csvfile.close() #Close the CSV File.
     print(str(index) + "/" + str(row_count) + " rows loaded from healthcare-dataset-stroke-data.csv", flush=True)
     return True
--- healthdb.sql
+++ healthdb.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS healthDB;
-CREATE DATABASE healthDB;
-USE healthDB;
-DROP TABLE IF EXISTS table_users;
+DROP DATABASE IF EXISTS healthDB; /*If the database already exists, drop it*/
+CREATE DATABASE healthDB; /*Create the database*/
+USE healthDB; /*Use the created database*/
+DROP TABLE IF EXISTS table_users; /*If table_users exists, drop it*/
 CREATE TABLE table_users(
     user_id INT NOT NULL,
     user_fullname TEXT NOT NULL,
@@ -10,9 +10,10 @@ CREATE TABLE table_users(
     user_phone TEXT NOT NULL,
     user_admin BOOLEAN NOT NULL,
     PRIMARY KEY (user_id)
-);
+); /*Create table_users and set the primary key to be the user_id*/
+/*This table holds all users*/
 
-DROP TABLE IF EXISTS table_patient_data;
+DROP TABLE IF EXISTS table_patient_data; /*If table_patient_data exists, drop it*/
 CREATE TABLE table_patient_data(
     patient_id INT NOT NULL,
     patient_gender TEXT NOT NULL,
@@ -27,17 +28,19 @@ CREATE TABLE table_patient_data(
     patient_smoked TEXT NOT NULL,
     patient_stroke BOOLEAN NOT NULL,
     PRIMARY KEY (patient_id) 
-);
+); /*Create table_patient_data and set the primary key to be patient_id*/
+/*This table holds all of the patient data*/
 
-DROP TABLE IF EXISTS link_user_patient_data;
+DROP TABLE IF EXISTS link_user_patient_data; /*If link_user_patient_data exists, drop it*/
 CREATE TABLE link_user_patient_data(
     user_id INT NOT NULL,
     patient_id INT NOT NULL,
     PRIMARY KEY (user_id),
     FOREIGN KEY(user_id) REFERENCES table_users(user_id),
     FOREIGN KEY(patient_id) REFERENCES table_patient_data(patient_id)
-);
+); /*Creae link_user_patient_data and set the primary key to be the user_id and link user_id and patient_id to their corresponding values in the other two tables*/
+/*This table holds all links between users and patients*/
 
 INSERT INTO table_users VALUES(
     0, "BaseAdmin", "-1", "baseadmin@example.com", "N/A", 1
-);
+); /*Create the BaseAdmin*/
