1077c74c09cd02472ad5d9f39e7e2a1021529ffe
mrhatman26 <{ID}+{username}@users.noreply.github.com>
2024-11-03 16:37:42 +0000

    Commented misc.py

--- misc.py
+++ misc.py
@@ -7,6 +7,9 @@ def is_number(text, check_float):
         return True
     except:
         return False
+    #This function checks if the given text is a number or not.
+    #text = The string to check. (String)
+    #check_float = If True, check if the string is a float instead. (Boolean)
     
 def str_to_booInt(YesNo):
     if is_number(YesNo, False):
@@ -16,6 +19,8 @@ def str_to_booInt(YesNo):
             return 1
         else:
             return 0
+    #A simple function to convert "Yes" "No" strings to their boolean counterparts.
+    #YesNo = The text to convert to a bool. (String)
     
 def clean_subdata(subdata):
 	subdata["patient_gender"] = subdata["patient_gender"].upper()
@@ -30,3 +35,5 @@ def clean_subdata(subdata):
 	subdata["patient_smoked"] = subdata["patient_smoked"].upper()
 	subdata["patient_stroke"] = int(subdata["patient_stroke"])
 	return subdata
+    #This function takes the subdata and puts it in the formats needed to be added to the database.
+    #subdata = A dictionary that contains the data to modify. (Dictionary)
